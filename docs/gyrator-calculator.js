bae9235f9c3b7ddadb41a17c109e83a32325347a
bunnegirl <github@bunnegirl.solutions>
2022-11-27 19:04:03 +1030

    removed filehash from build

--- docs/gyrator-calculator.js
+++ docs/gyrator-calculator.js
@@ -626,7 +626,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('gyrator-calculator-9f6b86013763843b_bg.wasm', import.meta.url);
+        input = new URL('gyrator-calculator_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
